20f5b89f65c322cce20ceede8c7029265bab12a2
vinodhconnects <vinodhconnects@gmail.com>
2023-06-12 12:54:24 +0530

    home-commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { service } from 'src/app/models/models';
+import { HomeService } from 'src/app/services/home.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
-
+export class HomeComponent implements OnInit {
+  services:service[] = []
+  constructor(private hs:HomeService) {}
+  ngOnInit(): void {
+      this.hs.getServices().subscribe(
+         {
+           next: (data:service[]) => this.services = data,
+           error: (error:any) => this.services = []
+         }
+      )
+  }
 }
