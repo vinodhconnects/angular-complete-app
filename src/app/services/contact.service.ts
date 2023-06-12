b4470dbcc924e2f5a862f185ad2cb22208e8dfc7
vinodhconnects <vinodhconnects@gmail.com>
2023-06-12 10:51:17 +0530

    service-commit

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
+//to use httpClient we need to inject HttpClient 
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  getContacts():Observable<any>{
+     return this.http.get("http://localhost:4500/contacts")   
+  }
 }
