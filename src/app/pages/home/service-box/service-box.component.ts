6dfe2cffff88a2ea87d515f7367452c2157be1ec
vinodhconnects <vinodhconnects@gmail.com>
2023-06-12 12:46:11 +0530

    css-commit

--- src/app/pages/home/service-box/service-box.component.ts
+++ src/app/pages/home/service-box/service-box.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { service } from 'src/app/models/models';
 
 @Component({
   selector: 'app-service-box',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./service-box.component.css']
 })
 export class ServiceBoxComponent {
-
+     @Input("service")  current:service = {
+        id: 0, description:"", image:"", title:""
+     }
 }
