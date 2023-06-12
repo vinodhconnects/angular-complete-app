ffdef45f0a2c219d4098482151754df206e2a715
vinodhconnects <vinodhconnects@gmail.com>
2023-06-12 11:23:26 +0530

    total

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { contact } from 'src/app/models/models';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit{
+    contacts:contact[]=[];
+
+    constructor(private cs:ContactService ){
+
+    }
+
+     ngOnInit(): void {
+           this.cs.getContacts().subscribe(
+             {
+               next: (data:contact[]) => this.contacts =data,
+               error: (error:any) => this.contacts =[]
+             }
+           ) 
+
+     }
 
 }
