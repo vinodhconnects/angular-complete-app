5b70866a51ff6346f962082882d1f8d754daf2bc
vinodhconnects <vinodhconnects@gmail.com>
2023-06-15 12:56:54 +0530

    validation

--- src/app/pages/reviews/review-form/review-form.component.ts
+++ src/app/pages/reviews/review-form/review-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ReviewService } from 'src/app/services/review.service';
 
 @Component({
   selector: 'app-review-form',
@@ -12,4 +13,22 @@ export class ReviewFormComponent {
   message:string=""
   types:string[]=["Branding and Communication","Inbound &  Content Marketing",
   "Film &  Webinar","Digital Marketing","ECommerce & Websites","Market Analysis"]
+  errors:string[]= []
+
+  constructor(private rs:ReviewService) {
+
+  }
+
+  postReview():void {
+    this.errors = []
+    let regex=/^[a-z][a-z\._0-9]{3,}@[a-z]{3,15}\.(com|net|org|in)$/
+    if(this.name.length<3)
+       this.errors.push("Name should be atleast three characters long")
+    if(regex.test(this.email))
+       this.errors.push("Email Must be in email format")
+    if(this.type=="")
+       this.errors.push("Please select a project type")
+    if(this.message.length<20)
+       this.errors.push("Message should be atleast 20 characters long")
+  }
 }
