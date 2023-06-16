b58f623c50b6c0795d1e9e3f26c4be4d770fdfa7
vinodhconnects <vinodhconnects@gmail.com>
2023-06-16 13:17:10 +0530

    fresh-commit

--- src/app/pages/home/enquiry-form/enquiry-form.component.ts
+++ src/app/pages/home/enquiry-form/enquiry-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder,Validators } from '@angular/forms';
+import { HomeService } from 'src/app/services/home.service';
 
 @Component({
   selector: 'app-enquiry-form',
@@ -7,8 +9,22 @@ import { Component } from '@angular/core';
 })
 export class EnquiryFormComponent {
   ftoggle:boolean=false
-
+  eform:any
+  constructor(private fb:FormBuilder,private hs:HomeService){
+  this.eform= this.fb.group(
+    {
+      name:['',[Validators.required,Validators.minLength(3)]],
+      description:['',[Validators.required,Validators.minLength(20)]],
+      email:['',Validators.email],
+      subject:['',[Validators.required,Validators.minLength(5)]],
+      supportfile: [null,[]]
+    }
+  )
+ 
+  }
   postEnquiry():void {
 
   }
+
+
 }
